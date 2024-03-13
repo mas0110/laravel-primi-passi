8d34f189d380d3881856043debec3e49abbcc8e2
mas0110 <masoudmohammed13@gmail.com>
2024-03-13 09:39:01 +0100

    nuova rotta con  e passaggio del dato dinamico

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/', function () {
+    // return view('welcome');
+// });
+
 Route::get('/', function () {
-    return view('welcome');
+
+    $data = [
+        'word' => 'ciao'
+    ];
+    
+    return view('Homepage', compact( 'data' ));
 });
+
